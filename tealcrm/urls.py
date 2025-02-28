9cc889b7ec721f9782eac039ca5ea32644101a9c
george <nccleb@gmail.com>
2025-02-28 16:35:48 +0200

    3rd commit

--- tealcrm/urls.py
+++ tealcrm/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.contrib.auth import views
 from django.urls import include, path
 from core.views import UserLogoutView, about, index
+from dashboard.views import dashboard
 from userprofile.views import signup, myaccount
 from userprofile.forms import LoginForm
 from django.contrib.auth.views import LogoutView
@@ -16,7 +17,8 @@ urlpatterns = [
     path('signup/',signup,name='signup'),
     path('log-in/', views.LoginView.as_view(template_name='userprofile/login.html', authentication_form=LoginForm), name='login'),
     path('logout/', UserLogoutView.as_view(http_method_names = ['get', 'post', 'options'] ), name='logout'),
-    path('dashboard/', include('dashboard.urls')),
+    #path('dashboard/', include('dashboard.urls')),
+    path('dashboard/',dashboard,name='dashboard'),
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
