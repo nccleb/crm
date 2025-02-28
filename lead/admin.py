c72c1c13241997de0e9e711e594d9daf21490600
george <nccleb@gmail.com>
2025-02-28 21:18:00 +0200

    lead

---
+++ lead/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Lead
+
+admin.site.register(Lead)
+#admin.site.register(Comment)
+#admin.site.register(LeadFile)
--- tealcrm/urls.py
+++ tealcrm/urls.py
@@ -8,7 +8,7 @@ from dashboard.views import dashboard
 from userprofile.views import signup, myaccount
 from userprofile.forms import LoginForm
 from django.contrib.auth.views import LogoutView
-
+from lead.views import add_lead, leads_list,leads_detail,leads_delete,leads_edit
 
 urlpatterns = [
     path('about/',about,name='about'),
@@ -17,8 +17,13 @@ urlpatterns = [
     path('signup/',signup,name='signup'),
     path('log-in/', views.LoginView.as_view(template_name='userprofile/login.html', authentication_form=LoginForm), name='login'),
     path('logout/', UserLogoutView.as_view(http_method_names = ['get', 'post', 'options'] ), name='logout'),
-    #path('dashboard/', include('dashboard.urls')),
+    
     path('dashboard/',dashboard,name='dashboard'),
+    path('add_lead/', add_lead, name='add_lead'),
+    path('leads_list/',leads_list, name='leads_list' ),
+    path('<int:pk>/', leads_detail, name='leads_detail'),
+    path('delete/<int:pk>/', leads_delete, name='leads_delete'),
+    path('edit/<int:pk>/', leads_edit, name='leads_edit'),
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
