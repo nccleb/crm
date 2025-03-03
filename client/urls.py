b9851c5a7ede2a18275b87660d1f4071b6ef0791
george <nccleb@gmail.com>
2025-03-03 23:49:10 +0200

    client 2

--- client/urls.py
+++ client/urls.py
@@ -6,6 +6,9 @@ from . import views
 urlpatterns = [
     
     path('', views.clients_list, name='clients_list'),
-    path('<int:pk>', views.clients_detail, name='clients_detail'),
-     
+    path('<int:pk>/', views.clients_detail, name='clients_detail'),
+    path('<int:pk>/delete', views.clients_delete, name='clients_delete'),
+    path('edit/<int:pk>', views.clients_edit, name='clients_edit'),
+    #path('edit/', views.clients_list, name='clients_list'),
+    path('add/', views.clients_add, name='clients_add'),
 ]
--- client/views.py
+++ client/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import get_object_or_404, render
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import Client
+from .forms import AddClientForm
 
 @login_required
 def clients_list(request):
@@ -20,3 +22,56 @@ def clients_detail(request,pk):
     return render(request, 'client/clients_detail.html', {
         'client': client
     })
+
+
+@login_required
+def clients_add(request):
+   if request.method =='POST':  
+     form = AddClientForm(request.POST)
+     if form.is_valid():
+            client = form.save(commit=False)
+            
+            
+            client.created_by = request.user
+            client.save()
+            messages.success(request, f"the client was created!")
+     return redirect('clients_list')
+   else:
+     form = AddClientForm() 
+     
+   return render(request, 'client/clients_add.html',{
+    'form':form
+ })
+
+@login_required
+def clients_edit(request, pk):
+    
+    client = get_object_or_404(Client, created_by=request.user, pk=pk)
+
+    if request.method == 'POST':
+        form = AddClientForm(request.POST, instance=client)
+
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, 'The changes was saved.')
+
+            return redirect('clients_list')
+    else:
+        form = AddClientForm(instance=client)
+    
+    return render(request, 'client/clients_edit.html', {
+        'form': form
+    })
+
+
+
+
+@login_required
+def clients_delete(request,pk):
+    client = get_object_or_404(Client, created_by=request.user, pk=pk)
+    client.delete()
+    messages.success(request, f"the client was deleted!")
+    return redirect('clients_list')
+
+
